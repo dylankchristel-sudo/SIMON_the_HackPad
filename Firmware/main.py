ba6a6b0058fb62f9a9920723fb1d4f95ea564aac
dylankchristel-sudo <dylankchristel@gmail.com>
2026-06-26 17:36:19 -0500

    changed pin names on firmware

--- Firmware/main.py
+++ Firmware/main.py
@@ -1,21 +1,82 @@
-//idk what im doing this is my best atempt until I have time to figure it out properly
-
 import board
-from kmk.kmk_keyboard Import KMKKeyboard
-from kmk.kays import KC
-from kmk.scanner import MatrixScanner
-from kmk.scanners import DiodeOrientation
+import busio
+import adafruit_ssd1306
 
-Keyboard = KMKKeyBoard()
+from kmk.kmk_keyboard import KMKKeyboard
+from kmk.keys import KC
+from kmk.modules.layers import Layers
+from kmk.scanners.keypad import keysScanner
+from kmk.modules.macros import Press, Release, Tap, Macros
+from kmk.modules.encoder import EncoderHandler
+from kmk.extentions.rgb import RGB
+from kmk.extentions.display import Display, TestEntry
 
-keyboard.col_pins = ('GPIO01', 'GPIOo2', 'GPIO03', 'GPIO04', 'GPIO05', 'GPIO06', 'GPIO43', 'GPIO09', GPIO08', 'GPIO07', 'GPIO44')
-keyboard.row_pins = () 
+keyboard = KMKKeyboard()
 
-keyboard.diode_orientation = DiodeOrientation.COLUMNS
+macros = Macros()
+keyboard.modules.append(macros)
+
+encoder = EncoderHandler()
+keyboard.modules.append(encoder_handler)
+
+keyboard.matrix = KeysScanner(
+    pins=[board.GP4, board.GP2, board.GP27 #sw2, sw3
+        board.GP26, board.GP3, board.GP1,  #sw6, sw1, sw4
+]
+value_when_pressed-pressed=False,
+)
+
+rgb = RGB(
+    Pisel_pin=board.6
+    num_pixels=4,
+    val=150
+    )
+    keyboard.extentions.append(rgb)
+
+encoder_handler.pins = (
+    (board.GP6,  board.GP28, None, False)
+)
+
+i2c_bus = busio.I2C(
+    scl=board.GP6,
+    sda=board.GP7
+)
+
+display.text + [
+    TextEntry(text='HELLO!', x=0, y=0)
+    TextEntry(text='Layer(Ihardlyknowher)0', x=0, y=12)
+]
+oled = Display(
+    display=adafruit_ssd1306.SSD1306_I2C(
+        128,
+        32,
+        i2c_bus
+    ),
+    entries=display_text
+)
+keyboard.extentions.append(oled)
 
 keyboard.keymap = [
-	[KC.ESCAPE, KC.LALT, KC.LSHIFT, KC.RCTRL]
+[
+    KC.MACRO(Press(KC.D),),
+    
+    KC.MACRO(Press(KC.Y),),
+
+    KC.MUTE,
+    
+    KC.MACRO(Press(KC.L),),
+    
+    KC.MACRO(Press(KC.A)),
+    
+    KC.MACRO(Press(KC.RGB_TOG)),
+
+    
+],
 ]
 
-if__name__ =='__main__':
-	keyboard.go()
+encoder_handler.map = [
+    ((KC.VOLD, KC.VOLU),)
+]
+
+if __name__ == '__main__':
+    keyboard.go()
